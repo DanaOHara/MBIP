ffc8b25a5fd1d680db419d473f3843a0063c67d2
DanaOHara <rosa.munozv@usach.cl>
2019-04-17 01:23:45 -0400

    FIX mailer paramétrico

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,8 +50,8 @@ class EventsController < ApplicationController
     def eventosDetalle
 
       #@events = Event.select(:name, :description, :timestart, :timemodified, :id).where('id = ?', params[:eventid])
-        @events = Event.select(:name, :description, :timestart, :timemodified, :id,'usr.email').joins(" INNER JOIN user usr ON event.userid = usr.id"). where("event.id = ? ", params[:eventid])
-      return @events
+      @events = Event.select(:name, :description, :timestart, :timemodified, :id,'usr.email').joins(" INNER JOIN user usr ON event.userid = usr.id"). where("event.id = ? ", params[:eventid])
+      #return @events
       #render json: @events
     end
 
@@ -73,9 +73,9 @@ class EventsController < ApplicationController
 
     def enviarCorreo
       #name = params[:name]
-      email = params[:email]
+      email = params[:event][:email]
       #body = params[:body]
-      EventMailer.assignment_email(email).deliver
+      EventMailer.assignment_email(email).deliver_now!
       redirect_to events_path
     end
 
